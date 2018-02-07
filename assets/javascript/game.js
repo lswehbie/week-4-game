5c9c3bb8476e417b002578ffec04109c0a91f066
Wehbiels <Lara.Wehbie@gmail.com>
2018-02-07 14:45:11 -0500

    Week-4-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,44 @@ var crystalThree = Math.floor((Math.random() * (12 - 1 + 1) + 1));
 
 var crystalFour = Math.floor((Math.random() * (12 - 1 + 1) + 1));
 
+
+$(document).ready(function() {
+
+	var crystalImageOne = $("#crystalOne");
+	var crystalImageTwo = $("#crystalTwo");
+	var crystalImageThree = $("#crystalThree");
+	var crystalImageFour = $("#crystalFour");
+
 // counter adds up numbers to equal random number generated 
+
+	var counter = 0;
+
+// If we click crystal image one the counter will increase by crystal one (random number), 
+// and if we click crystal image two it will increase the counter by crystal two,
+// and if we click crystal image three it will increase the counter by crystal three,
+// and if we click crystal image four it will increase the counter by crystal four.
+
+	$(crystalImageOne).click(function() {
+		counter += crystalOne
+	});
+
+	$(crystalImageTwo).click(function() {
+		counter += crystalTwo
+	});
+
+	$(crystalImageThree).click(function() {
+		counter += crystalThree
+	});
+
+	$(crystalImageFour).click(function() {
+		counter += crystalFour
+	});
+
+
+
+});
+
+
 
 // win count / lose count
 
