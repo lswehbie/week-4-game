b04872dc6f671462ec02b2a14e496e84b22c0f11
Wehbiels <Lara.Wehbie@gmail.com>
2018-02-07 15:33:03 -0500

    Crystal Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,82 @@
-// random number generated 
+//putting reset function outside of document.ready to act as a global function.
 
-var givenRandomNumber = Math.floor((Math.random() * (120 - 19 + 1) + 19));
+function reset() {
+	// random number generated
 
-// four crystals that represent a hidden number
+	var givenRandomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
 
-var crystalOne = Math.floor((Math.random() * (12 - 1 + 1) + 1));
+	// four crystals that represent a hidden number
 
-var crystalTwo = Math.floor((Math.random() * (12 - 1 + 1) + 1));
+	var crystalOne = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 
-var crystalThree = Math.floor((Math.random() * (12 - 1 + 1) + 1));
+	var crystalTwo = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 
-var crystalFour = Math.floor((Math.random() * (12 - 1 + 1) + 1));
+	var crystalThree = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 
+	var crystalFour = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+
+	// counter adds up numbers to equal random number generated
+	var counter = 0;
+}
 
 $(document).ready(function() {
-
 	var crystalImageOne = $("#crystalOne");
 	var crystalImageTwo = $("#crystalTwo");
 	var crystalImageThree = $("#crystalThree");
 	var crystalImageFour = $("#crystalFour");
 
-// counter adds up numbers to equal random number generated 
+	var winCount = 0;
+	var loseCount = 0;
 
-	var counter = 0;
-
-// If we click crystal image one the counter will increase by crystal one (random number), 
-// and if we click crystal image two it will increase the counter by crystal two,
-// and if we click crystal image three it will increase the counter by crystal three,
-// and if we click crystal image four it will increase the counter by crystal four.
+	// If we click crystal image one the counter will increase by crystal one (random number),
+	// and if we click crystal image two it will increase the counter by crystal two,
+	// and if we click crystal image three it will increase the counter by crystal three,
+	// and if we click crystal image four it will increase the counter by crystal four.
 
 	$(crystalImageOne).click(function() {
-		counter += crystalOne
+		counter += crystalOne;
+		counterCheck(counter, givenRandomNumber);
 	});
 
 	$(crystalImageTwo).click(function() {
-		counter += crystalTwo
+		counter += crystalTwo;
+		counterCheck(counter, givenRandomNumber);
 	});
 
 	$(crystalImageThree).click(function() {
-		counter += crystalThree
+		counter += crystalThree;
+		counterCheck(counter, givenRandomNumber);
 	});
 
 	$(crystalImageFour).click(function() {
-		counter += crystalFour
+		counter += crystalFour;
+		counterCheck(counter, givenRandomNumber);
 	});
 
+	// everytime we click on an image and the counter increases--
+	// we should check if the counter value is equal to the given random number.
+	// if that is true, then you WIN!! and the game restarts.
+	// if else you LOSE and the game restarts and new values are assigned.
+	// if the number is greater than the random given number you lose.
+	// else you keep playing.
 
+	// game restarts whenever the player wins or loses.
 
+	function counterCheck(currentTotal, total) {
+		if (currentTotal === total) {
+			winCount += 1;
+		} else if (currentTotal > total) {
+			loseCount += 1;
+		}
+
+		// win count / lose count
+
+		// if the current total is equal to the given random number you win! Increase win count
+		//and restart game.
+
+		// if the current total is greater than the random given number then we increase the lose count.
+		//and restart the game.
+	}
 });
-
-
-
-// win count / lose count
 
 // game restarts whenever the player wins or loses.
