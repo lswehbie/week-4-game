e575f84a0d5484a03b4e71e12824a6fd6bb89690
Wehbiels <Lara.Wehbie@gmail.com>
2018-02-07 13:46:37 -0500

    week-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@
 
 var givenRandomNumber = Math.floor((Math.random() * (120 - 19 + 1) + 19));
 
+// four crystals that represent a hidden number
+
 var crystalOne = Math.floor((Math.random() * (12 - 1 + 1) + 1));
 
 var crystalTwo = Math.floor((Math.random() * (12 - 1 + 1) + 1));
@@ -9,12 +11,6 @@ var crystalTwo = Math.floor((Math.random() * (12 - 1 + 1) + 1));
 var crystalThree = Math.floor((Math.random() * (12 - 1 + 1) + 1));
 
 var crystalFour = Math.floor((Math.random() * (12 - 1 + 1) + 1));
-
-
-
-
-
-// four gems that represent a number
 
 // counter adds up numbers to equal random number generated 
 
